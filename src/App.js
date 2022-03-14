f369cac2701524b5f4a19a2ccca5494ab0633d18
Mejsun <mejsunalghoul@gmail.com>
2022-03-14 14:56:31 +0000

    stop-pause functioning and app coutning down

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
   const [seconds, setSeconds] = useState(0);
-  const [pause, setPause] = useState(false)
+  const [minutes, setMinutes] = useState(25);
+  const [pause, setPause] = useState(true);
+  const [timeLeft, setTimeLeft] = useState([minutes, seconds])
+  const [tracker, setTracker] = useState('Session')
 
   const handleBrIncrement = ()=> {
     if(breakLength >= 0 && breakLength < 60){
@@ -13,7 +16,7 @@ function App() {
     }
   }
   const handleBrDecrement = ()=> {
-    if(breakLength >= 1 && breakLength < 60){
+    if(breakLength > 1 && breakLength < 60){
     setBreakLength(breakLength-1)
     }
   }
@@ -23,41 +26,78 @@ function App() {
     }
   }
   const handleSeDecrement = () => {
-    if(sessionLength >= 1 && sessionLength < 60){
+    if(sessionLength > 1 && sessionLength < 60){
     setSessionLength(sessionLength-1)
     }
   }
   const clearAll = () => {
     setBreakLength(5)
     setSessionLength(25)
+    setSeconds(0)
+    setMinutes(25)
+    setPause(true)
   }
 
   const togglePause = () => {
     setPause(!pause)
+    console.log(timeLeft)
   }
+
   useEffect(() => {
+    
     let interval;
-    if (pause) {
-      interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
+
+    if (!pause) {
+    interval = setInterval(() => {
+        if(seconds > 0 && sessionLength >= 0){
+          setSeconds(seconds - 1);
+          setMinutes(sessionLength)
+          setTracker('Session')
+        
+        } else if (seconds === 0 && sessionLength > 0){
+          setSeconds(60  - 1)
+          setMinutes(sessionLength - 1);
+          setTracker('Session')
+        
+        }else if (seconds === 0 && sessionLength === 0){
+          setSeconds(60  - 1)
+          setMinutes(breakLength);
+          setTracker('Break')
+        
+        }else if (seconds === 0 && breakLength > 0){
+          setSeconds(60  - 1)
+          setMinutes(breakLength - 1);
+          setTracker('Break')
+        
+        }else if(seconds > 0 && breakLength >= 0){
+          setSeconds(seconds - 1);
+          setMinutes(breakLength)
+          setTracker('Break')
+        
+        }else if (seconds === 0 && breakLength === 0){
+          setSeconds(60  - 1)
+          setMinutes(sessionLength);
+          setTracker('Session')
+        }
+        
       }, 1000);
-    } else if (!pause && seconds !== 0) {
-      clearInterval(interval);
     }
+      else if (pause && setMinutes !== 0) {
+        clearInterval(interval);
+      }
     return () => clearInterval(interval);
-  }, [pause, seconds]);
 
 
-  const timeLeft = ((sessionLength).toString().padStart(2, '0')+ ':'+ (seconds).toString().padStart(2, '0'))
+
+  }, [pause, seconds, breakLength, sessionLength, ]);
 
   return (
     <div className="App">
      
-      <span id="timer-label">Session or Break</span>
-      <span id="time-left">{timeLeft}</span>
+      <span id="timer-label">{tracker}</span>
+      <span id="time-left">{`${(minutes.toString().padStart(2, '0'))}:${(seconds.toString().padStart(2, '0'))}`}</span>
       <button id="start_stop" onClick={togglePause}>start-stop</button>
       <button id="reset" onClick={clearAll}>reset</button>
-
 
       <h2 id="break-label">Break length</h2>
       <span id="break-length">{breakLength}</span>
